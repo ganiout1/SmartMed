69a93ee885056fc319b01fdc9ce823abab9c8b1d
ganijack <ganijack24@gmail.com>
2026-07-28 08:25:03 +0800

    style: double mobile hero logo size to 240px

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -181,7 +181,7 @@ export function HeroSection() {
 
         {/* RIGHT: Logo - absolute on mobile, grid column on desktop */}
         <div
-          className="absolute top-[40px] right-[16px] w-[120px] lg:relative lg:top-auto lg:right-auto lg:w-auto lg:flex lg:justify-center lg:items-center lg:translate-x-[250px] lg:translate-y-[-300px]"
+          className="absolute top-[-30px] right-[16px] w-[240px] lg:relative lg:top-auto lg:right-auto lg:w-auto lg:flex lg:justify-center lg:items-center lg:translate-x-[250px] lg:translate-y-[-300px]"
           style={{ overflow: "visible", zIndex: 2 }}
         >
           <Image
@@ -189,7 +189,7 @@ export function HeroSection() {
             alt="SmartMED"
             width={3040}
             height={912}
-            className="w-[120px] lg:w-[1520px]"
+            className="w-[240px] lg:w-[1520px]"
             style={{ objectFit: "contain", height: "auto", flexShrink: 0 }}
             priority
           />
