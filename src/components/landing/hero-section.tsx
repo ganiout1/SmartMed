5d7a92b44dd3f44a0324f31367bef3d929398c64
ganijack <ganijack24@gmail.com>
2026-07-28 09:21:11 +0800

    style: shift tablet hero logo up and to the right

--- src/components/landing/hero-section.tsx
+++ src/components/landing/hero-section.tsx
@@ -181,7 +181,7 @@ export function HeroSection() {
 
         {/* RIGHT: Logo - absolute on mobile, grid column on desktop */}
         <div
-          className="absolute top-[-60px] right-[-5px] w-[168px] md:top-[-120px] md:right-[15px] md:w-[364px] lg:relative lg:top-auto lg:right-auto lg:w-auto lg:flex lg:justify-center lg:items-center lg:translate-x-[250px] lg:translate-y-[-300px]"
+          className="absolute top-[-60px] right-[-5px] w-[168px] md:top-[-160px] md:right-[-20px] md:w-[364px] lg:relative lg:top-auto lg:right-auto lg:w-auto lg:flex lg:justify-center lg:items-center lg:translate-x-[250px] lg:translate-y-[-300px]"
           style={{ overflow: "visible", zIndex: 2 }}
         >
           <Image
